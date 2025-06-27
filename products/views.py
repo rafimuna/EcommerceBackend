a499eed756aec40200f0cd7f86cc04852e4d1ee5
rafimuna <rafimuna77@gmail.com>
2025-06-27 08:45:00 +0600

    updated views.py and make urls.py inside products app and added it in root/urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-id')
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'available']
+    search_fields = ['name']
