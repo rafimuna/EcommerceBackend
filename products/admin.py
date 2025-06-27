0d99f0c64bc8ae4df0e3339cf4e2cf9713c497e9
rafimuna <rafimuna77@gmail.com>
2025-06-27 08:48:25 +0600

    register models for the Django admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'price', 'available']
+    search_fields = ['name', 'category']
+    list_filter = ['available', 'category']
