ffb775829588297f4ef39919a35254efbd6762ea
rafimuna <rafimuna77@gmail.com>
2025-06-27 08:28:57 +0600

    create product class in models for create a product table

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
